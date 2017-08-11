f0172ee7563c09a9bbd2d195fed983cb0a142907
Matt Bumgardner <matt.bumgardner@gmail.com>
2017-08-11 13:35:40 -0500

    Inline picker updates from develop

--- WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
+++ WordPress/Classes/ViewRelated/Aztec/ViewControllers/AztecPostViewController.swift
@@ -1746,6 +1746,7 @@ extension AztecPostViewController : Aztec.FormatBarDelegate {
         picker.mediaPicker.viewControllerToUseToPresent = self
         picker.dataSource = WPPHAssetDataSource.sharedInstance()
         picker.mediaPicker.mediaPickerDelegate = self
+        picker.scrollVertically = true
 
         presentToolbarViewControllerAsInputView(picker)
     }
